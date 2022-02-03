9327118a7942a883e73028135a7aa54013adeade
darkniki <38648784+darkniki@users.noreply.github.com>
2022-02-03 17:52:49 +0300

    cookie alert (#13)
    * cookie alert
    
    * cookie alert
    
    * cookie alert

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,34 @@
   <div id="app">
     <header>
       <nav>
-        <LanguageSwitcher/>
+        <LanguageSwitcher />
       </nav>
     </header>
-    <router-view/>
+    <router-view />
+    <footer>
+      <cookie-law :buttonText="$t('cookiesButtonText')" theme="blood-red">
+        <div slot="message">
+          {{ $t('cookies') }}
+        </div>
+      </cookie-law>
+    </footer>
   </div>
 </template>
+
 <script>
 import {defineComponent, defineAsyncComponent} from '@vue/composition-api';
 
-const LanguageSwitcher = defineAsyncComponent( () => (import(/* webpackChunkName: "LanguageSwitcher" */'@/components/languageSwitcher/LanguageSwitcher')));
+const LanguageSwitcher = defineAsyncComponent(() => import(/* webpackChunkName: "LanguageSwitcher" */'@/components/languageSwitcher/LanguageSwitcher'));
+const CookieLaw = defineAsyncComponent(() => import(/* webpackChunkName: "CookieLaw" */'vue-cookie-law'));
 
 export default defineComponent({
-  components: {LanguageSwitcher},
+  components: {
+    LanguageSwitcher,
+    CookieLaw,
+  },
 });
 </script>
 
 <style lang="scss">
-  @import "src/assets/styles/main";
+@import "src/assets/styles/main";
 </style>
