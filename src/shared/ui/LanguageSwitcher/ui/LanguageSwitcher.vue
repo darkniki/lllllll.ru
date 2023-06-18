5b78f831ef6c604fe5c378a02f68a2d81ef6f149
Nikita Lapsar <darkniki777@gmail.com>
2023-06-18 20:15:00 +0300

    styles

--- src/shared/ui/LanguageSwitcher/ui/LanguageSwitcher.vue
+++ src/shared/ui/LanguageSwitcher/ui/LanguageSwitcher.vue
@@ -21,19 +21,37 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
-import { computed } from 'vue';
+import { computed, ref, watch } from 'vue';
+import Confetti from 'vue-confetti/src/confetti';
 import type { Locale } from '@/shared/config/i18n';
 import { Translation } from '@/shared/config/i18n';
 
 const { locale, availableLocales } = useI18n();
 const router = useRouter();
 const filteredLanguages = computed(() => availableLocales.filter((lng) => lng !== locale.value));
+const clickTimes = ref(0);
+
 const switchLanguage = async (newLocale: Locale) => {
+    clickTimes.value += 1;
     Translation.switchLanguage(newLocale);
     await router.push(
         { path: `/${newLocale}` },
     );
 };
+
+const confetti = new Confetti();
+const isConfetti = ref(false);
+
+const switchConfetti = () => {
+    if (isConfetti.value) {
+        confetti.stop();
+        isConfetti.value = false;
+    } else {
+        confetti.start();
+        isConfetti.value = true;
+    }
+};
+watch(clickTimes, (value) => (value % 7 === 0 ? switchConfetti() : null));
 </script>
 <style scoped lang="scss">
 @import "./LanguageSwitcher";
